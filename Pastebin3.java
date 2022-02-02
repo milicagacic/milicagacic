2c25ced28839549126f3f98e42054df16dab5ef2
Milica Gacic <97267945+milicagacic@users.noreply.github.com>
2022-02-02 01:23:39 +0100

    Add files via upload

---
+++ Pastebin3.java
@@ -0,0 +1,26 @@
+package Pastebin3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* Za unet broj n, ispisati "Pozitivan" ako je strogo veci od 0,
+        "Negativan" ako je strogo manji od 0 ili "Neutralan" ako je tacno 0.
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Unesite neki ceo broj");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            System.out.println("Pozitivan.");
+        } else if (n < 0) {
+            System.out.println("Negativan.");
+        } else if (n == 0) {
+            System.out.println("Neutralan.");
+        }
+
+    }
+}
